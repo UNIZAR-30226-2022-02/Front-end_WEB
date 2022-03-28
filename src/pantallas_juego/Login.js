f732afc6ef32480b583bea2b43dde51651479635
javi2715 <javiervallesdemiguel@gmail.com>
2022-03-28 20:57:24 +0200

    Cambios Estructura Inicio.js

--- src/pantallas_juego/Login.js
+++ src/pantallas_juego/Login.js
@@ -1,64 +1,60 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import logo_risk from '../imagenes/logo_risk.png';
+import styled from 'styled-components';
+import { Button, Container, Form, FormGroup, Input } from 'reactstrap';
+
+import imagen_fondo from '../imagenes/fondo_pantalla.jpeg';
 
 class Inicio extends React.Component{
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      nombre: "",
-      password: ""
-    };
-
-    this.handleNombre = this.handleNombre.bind(this);
-    this.handlePassword = this.handlePassword.bind(this);
+  handleLogin(event) {
+    
   }
-
-
-  handleNombre(event) {
-    this.setState(
-      {nombre: event.target.Value}
-    )
-  };
-
-  handlePassword(event) {
-    this.setState (
-      {password: event.target.value}
-    )
-  };
 
   render() {
     return (
-      <div style={{textAlign: "center"}} className="Inicio">
-
-        <div>
-          <img style={{marginTop: "20px", width:"50%", height:"35%"}} alt="Remy Sharp" className="Logo_Risk" src={logo_risk}></img>
-        </div>
-
-        <div style={{marginLeft: "25%", width: "50%", marginTop: "30px"}} class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInput" placeholder="Nombre Usuario"
-            value={this.state.nombre} onChange={this.handleNombre}></input>
-          <label for="floatingInput">Nombre usuario</label>
-        </div>
-
-        <div style={{marginLeft: "25%", width: "50%", marginTop: "30px"}} class="form-floating mb-3">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña"
-            value={this.state.password} onChange={this.handlePassword}></input>
-          <label for="floatingPassword">Password</label>
-        </div>
-
-        <br></br>
-
-        <button type="button" class="btn btn-outline-primary btn-lg">Inicar sesión</button>
-
-        <Link to="/registro">
-          <button type="button" class="btn btn-outline-info btn-lg">Regístrate</button>
-        </Link>
-
-      </div>
+      <BackGroundImage>
+        <MainContainer>
+          <Logo />
+          <Form onSubmit={this.handleLogin}>
+            <FormGroup>
+              <Input type="email" name="name" placeholder="Email"/>
+            </FormGroup>
+            <FormGroup>
+              <Input type="password" name="password" placeholder="Password"/>
+            </FormGroup>
+            <ButtonLogin>Iniciar sesión</ButtonLogin>
+          </Form>
+          <ButtonRegister href="/registro"></ButtonRegister>
+        </MainContainer>
+      </BackGroundImage>
     );
   }
 };
+
+const BackGroundImage = styled.div`
+  background-image: url(${imagen_fondo});
+  height: 100%
+  width: 100%;
+  background-position: center;
+  background-size: cover;
+`;
+
+const MainContainer = styled(Container)`
+  align-items: center;
+  text-align: center;
+  
+`;
+
+const Logo = styled.div`
+
+`;
+
+const ButtonLogin = styled(Button)`
+
+`;
+
+const ButtonRegister = styled(Button)`
+
+`;
 
 export default Inicio;
